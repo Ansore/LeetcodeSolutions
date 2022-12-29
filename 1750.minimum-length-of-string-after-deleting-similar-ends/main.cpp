fb0042be5f2920b1ab6bb5a911a5d481f6a1ce85
Ansore <aansore@outlook.com>
2022-12-29 13:09:56 +0800

    1750

---
+++ 1750.minimum-length-of-string-after-deleting-similar-ends/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int minimumLength(string s) {
+    int i = 0, j = s.length() - 1;
+    char l, r;
+    while (i < j && s[i] == s[j]) {
+      l = s[i], r = s[j];
+      while (i < j && s[i] == l) {
+        i++;
+      }
+      while (i - 1 < j && s[j] == r) {
+        j--;
+      }
+    }
+    return j - i + 1;
+  }
+};
