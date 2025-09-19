0b49c6d2ec95f29d26038db3976205b886960455
Ansore <aansore@outlook.com>
2025-09-19 22:34:45 +0800

    1935

---
+++ 1935.maximum-number-of-words-you-can-type/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int canBeTypedWords(string text, string brokenLetters) {
+    int sum = 0;
+    int b_sum = 0;
+    bool flag = true;
+    for (char t : text) {
+      if (t == ' ') {
+        sum++;
+        flag = true;
+        continue;
+      }
+      for (char b : brokenLetters) {
+        if (flag && b == t) {
+          b_sum++;
+          flag = false;
+        }
+      }
+    }
+    sum++;
+    return sum - b_sum;
+  }
+};
